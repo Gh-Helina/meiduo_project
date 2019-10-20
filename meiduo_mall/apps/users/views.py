7518900088cd5b105d9f57db7fbf5ff97b7e75ef
helina <hln@163.com>
2019-10-20 15:01:43 +0800

    邮件验证

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -14,7 +14,6 @@ from apps.users.models import User
 from utils.response_code import RETCODE
 
 
-
 class RegisterView(View):
     """用户注册"""
 
@@ -120,9 +119,9 @@ class LoginVies(View):
         else:
             # 不记住登录，关闭浏览器失效
             request.session.set_expiry(0)
-        # return redirect(reverse('contents:index'))
+            # return redirect(reverse('contents:index'))
 
-##############首页用户名展示#######################
+        ##############首页用户名展示#######################
         # 响应注册结果
         response = redirect(reverse('contents:index'))
 
@@ -130,6 +129,8 @@ class LoginVies(View):
         response.set_cookie('username', user.username, max_age=3600 * 24 * 14)
 
         return response
+
+
 #
 #     """
 # 1.功能分析
@@ -146,76 +147,93 @@ class LoginVies(View):
 # """
 #
 class LogoutView(View):
-#
-    def get(self,request):
-#
-#         # request.session.flush()
-#
+    #
+    def get(self, request):
+        #
+        #         # request.session.flush()
+        #
         from django.contrib.auth import logout
         logout(request)
-#
-#         #删除cookie中的username
-#         return redirect(reverse('contents:index'))
-        response =  redirect(reverse('contents:index'))
-#
-#         # response.set_cookie('username',None,max_age=0)
+        #
+        #         #删除cookie中的username
+        #         return redirect(reverse('contents:index'))
+        response = redirect(reverse('contents:index'))
+        #
+        #         # response.set_cookie('username',None,max_age=0)
         response.delete_cookie('username')
-#
+        #
         return response
 
-
-###########判断是否登录############
-# class UserCenterInfoView(View):
-#     def get(self,request):
-#       request.user 请求中 有用户的信息
-        # is_authenticated 判断用户是否为登陆用户
-        # 登陆用户为True
+    ###########判断是否登录############
+    # class UserCenterInfoView(View):
+    #     def get(self,request):
+    #       request.user 请求中 有用户的信息
+    # is_authenticated 判断用户是否为登陆用户
+    # 登陆用户为True
     # 未登陆用户为False
-        # if request.user.is_authenticated:
-        #     return render(request,'user_center_info.html')
-        # else:
-        #     return redirect(reverse('users:login'))
-#         return render(request,'user_center_info.html')
-#第二种
+    # if request.user.is_authenticated:
+    #     return render(request,'user_center_info.html')
+    # else:
+    #     return redirect(reverse('users:login'))
+
+
+# return render(request,'user_center_info.html')
+# 第二种
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class UserCenterInfoView(LoginRequiredMixin,View):
 
-    def get(self,request):
+class UserCenterInfoView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'user_center_info.html')
 
-
-        return render(request,'user_center_info.html')
-#########用户中心代码###############
-    def get(self,request):
+    #########用户中心代码###############
+    def get(self, request):
         # 1.获取用户登录信息
-        context={'username':request.user.username,
-                'mobile':request.user.mobile,
-                'email':request.user.email,
-                 'email_active':request.user.email_active,
-                }
+        context = {'username': request.user.username,
+                   'mobile': request.user.mobile,
+                   'email': request.user.email,
+                   'email_active': request.user.email_active,
+                   }
         # 2.传递模型进行渲染
         return render(request, 'user_center_info.html', context=context)
 
 
 ############邮件验证##########
 # 必须登录用户,所以继承LoginRequiredMixin
-class EmailView(LoginRequiredMixin,View):
-    def put(self,request):
+class EmailView(LoginRequiredMixin, View):
+    def put(self, request):
         # 1.接收  axios
-        body=request.body
-        body_str=body.decode()
+        body = request.body
+        body_str = body.decode()
         # 导入系统json，最后一个json
-        data=json.loads(body_str)
+        data = json.loads(body_str)
 
         # 2.验证
-        email=data.get('email')
+        email = data.get('email')
         if not email:
             return HttpResponseBadRequest('缺少email参数')
         if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-            return JsonResponse({'code':RETCODE.PARAMERR,'errmsg':'邮箱不符合规则'})
+            return JsonResponse({'code': RETCODE.PARAMERR, 'errmsg': '邮箱不符合规则'})
         # 3.更新数据
-        request.user.email=email
+        request.user.email = email
         request.user.save()
         # 4.给邮箱发送激活链接
+        # 导入服务器
+        from django.core.mail import send_mail
+        # subject, message, from_email, recipient_list,
+        # subject        主题
+        subject = 'Forever激活邮件'
+        # message,       内容
+        message = ''
+        # from_email,  谁发的
+        from_email = '小姐姐<hln1369471@163.com>'
+        # recipient_list,  收件人列表
+        recipient_list = ['helina0329@163.com']
+
+        html_mesage = "<a href='http://www.huyouni.com'>戳我有惊喜</a>"
+        send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list, html_message=html_mesage)
+
         # 5.返回响应
-        return JsonResponse({'code':RETCODE.OK,'errmsg':'ok'})
+        return JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok'})
+
+
